38c6a364197019aaeacca17340ccdb05ee3ddf87
Lebnik <totx@ya.ru>
2024-04-11 12:20:52 +0300

    UrlManager->transliterate added

---
+++ src/UrlManager.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace YaPro\SeoBundle;
+
+class UrlManager
+{
+    private $translit = array(
+        'а' => 'a',
+        'б' => 'b',
+        'в' => 'v',
+        'г' => 'g',
+        'д' => 'd',
+        'е' => 'e',
+        'ё' => 'yo',
+        'ж' => 'j',
+        'з' => 'z',
+        'и' => 'i',
+        'й' => 'y',
+        'к' => 'k',
+        'л' => 'l',
+        'м' => 'm',
+        'н' => 'n',
+        'о' => 'o',
+        'п' => 'p',
+        'р' => 'r',
+        'с' => 's',
+        'т' => 't',
+        'у' => 'u',
+        'ф' => 'f',
+        'х' => 'h',
+        'ц' => 'c',
+        'ч' => 'ch',
+        'ш' => 'sh',
+        'щ' => 'sh',
+        'ъ' => 'i',
+        'ы' => 'i',
+        'ь' => 'i',
+        'э' => 'e',
+        'ю' => 'yu',
+        'я' => 'ya',
+        'є' => 'e',
+        'і' => 'i',
+        'ї' => 'yi');
+
+    // возвращает текст в транслитерированном виде, с заменой на символ _ всех символов кроме букв, цифр и тире
+    // Функция создана потому, что Transliterator::create('Any-Latin; Latin-ASCII')->transliterate($string); работает не так как хотелось.
+    public function transliterate(string $text): string
+    {
+        $text = mb_strtolower($text);
+        $text = strtr($text, $this->translit);
+        $text = preg_replace('/[^-a-z0-9]/sUi', '_', $text);
+        $text = preg_replace('/[\_]{2,}/', '_', $text);
+
+        return trim($text, '_');
+    }
+}
---
+++ tests/Unit/WhiteBox/UrlManagerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YaPro\SeoBundle\Tests\Unit\WhiteBox;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use YaPro\SeoBundle\UrlManager;
+
+class UrlManagerTest extends TestCase
+{
+    public function testTransliterate(): void
+    {
+        $object = new UrlManager();
+        $actual = $object->transliterate(' Привет Мир $! ');
+        $this->assertSame('privet_mir', $actual);
+    }
+}
