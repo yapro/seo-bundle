a05371b5f1fc60e33cc616d46a7a596b820e481f
Lebnik <totx@ya.ru>
2024-04-11 12:44:34 +0300

    cs

--- tests/Unit/WhiteBox/UrlManagerTest.php
+++ tests/Unit/WhiteBox/UrlManagerTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace YaPro\SeoBundle\Tests\Unit\WhiteBox;
 
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use YaPro\SeoBundle\UrlManager;
 
