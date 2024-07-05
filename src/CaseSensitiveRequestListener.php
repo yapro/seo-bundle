72b1e793ea890546ecc3167cd1d564d001ccc1ce
Lebnik <totx@ya.ru>
2024-07-05 07:35:20 +0000

    redirect logging

--- src/CaseSensitiveRequestListener.php
+++ src/CaseSensitiveRequestListener.php
@@ -4,22 +4,35 @@ declare(strict_types=1);
 
 namespace YaPro\SeoBundle;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 // если кто-то зайдет на страницу в которой будут символы в верхнем реигстре, его перенаправит на страницу с символами в нижнем регистре
 class CaseSensitiveRequestListener
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+    
     public function onKernelRequest(RequestEvent $event): void
     {
         if (!$event->isMainRequest()) {
             return;
         }
         $request = $event->getRequest();
+        $requestUri = $request->getRequestUri();
         $lowercasePath = mb_strtolower($request->getRequestUri());
-        if ($request->getRequestUri() === $lowercasePath) {
+        if ($requestUri === $lowercasePath) {
             return;
         }
+        $this->logger->notice('redirectToLowercase', [
+            'requestUri' => $requestUri,
+            'lowercasePath' => $lowercasePath,
+        ]);
         $event->setResponse(new RedirectResponse($request->getUriForPath($lowercasePath), 301));
     }
 }
